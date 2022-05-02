ea88c65920ab48993fa1bb4a16ed8ed706afa24f
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-05-01 21:17:30 -0500

    alembic, 2 database models, migration versions added

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import databases
 import sqlalchemy
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
-DATABASE_URL = "postgresql://postgres:luisfer123@localhost/store"
+DATABASE_URL = "postgresql://postgres:luisfer123@localhost/store"  # test DB
 
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
@@ -14,10 +14,20 @@ books = sqlalchemy.Table(
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column("title", sqlalchemy.String),
     sqlalchemy.Column("author", sqlalchemy.String),
+    sqlalchemy.Column("pages", sqlalchemy.String),
+    sqlalchemy.Column("reader_id", sqlalchemy.ForeignKey("readers.id"), nullable=False, index=True),
 )
 
-engine = sqlalchemy.create_engine(DATABASE_URL)
-metadata.create_all()
+
+readers = sqlalchemy.Table(
+    "readers",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("first_name", sqlalchemy.String),
+    sqlalchemy.Column("last_name", sqlalchemy.String),
+)
+# engine = sqlalchemy.create_engine(DATABASE_URL)
+# metadata.create_all(engine)
 
 app = FastAPI()
 
@@ -25,3 +35,22 @@ app = FastAPI()
 @app.on_event("startup")
 async def startup():
     await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.get("/books/")
+async def get_all_books():
+    query = books.select()
+    return await database.fetch_all(query)
+
+
+@app.post("/books/")
+async def create_book(request: Request):
+    data = await request.json()
+    query = books.insert().values(**data)
+    last_record_id = await database.execute(query)
+    return {"id": last_record_id}
